a18ebedcfac07bec34ece3e53e5ee1793ee915b1
Szymon Grzybowski <63613188+szymongrzybowski@users.noreply.github.com>
2022-09-13 15:08:19 +0200

    Update append_to_file.py

--- append_to_file.py
+++ append_to_file.py
@@ -7,7 +7,7 @@ my_list = ['Peter', 'Griffin']
 # csv.writer function
 with open('path/to/csv/file', 'a', newline='') as csvfile: # set 'a' mode
     csv_writer = csv.writer(csvfile, delimiter=',')
-    csv_writer.writerow(my_list)
+    csv_writer.writerows(my_list)
 
 columns = ['first_name', 'last_name']
 
